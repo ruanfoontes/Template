66d535457f6838f7ea40209c32c502b8207199df
Ruan fontes <ruanfontes309@gmail.com>
2026-04-13 23:47:43 -0300

    feat: Criado menu de opção

--- Main.java
+++ Main.java
@@ -6,5 +6,16 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         ArrayList<Tarefa> listaTarefas = new ArrayList<>();
+
+        int opcao = -1;
+        while (opcao != 0) {
+            opcao = scanner.nextInt();
+            System.out.println("---MENU---");
+            System.out.println("Digite uma Opção:");
+            System.out.println("Opção 0 Sair");
+            System.out.println("Opção 1 Adicionar Tarefa");
+            System.out.println("Opção 2 Listas Tarefa");
+            
+        }
     }
 }
